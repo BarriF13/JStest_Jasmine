f7e893a8392def87d221c072bdeadab15fa3e11b
Barri <info@bdesignstudio.co.uk>
2020-05-21 12:43:42 +0100

    first test series

---
+++ README.md
@@ -0,0 +1,3 @@
+
+JavaScript Jasmine Tutorial | QAShahin
+https://www.youtube.com/watch?v=g0q8pCsCB4o&list=PL_noPv5wmuO9op-OQ22SbHcqFGGHA6iIZ&index=2
--- spec/test.spec.js
+++ spec/test.spec.js
@@ -3,3 +3,23 @@ describe('hello world', function(){
 
   });
 });
+// describe = grouping test together
+// describe ('name of the test', function that will call back inside the describe block )
+//1--when we run the test function executed 
+//2-- it block is the test ('it should be able to add two numbers together', function())
+
+describe('calculate addition', function(){
+  it ('Should be able to add two numbers together', function(){
+    console.log('I was able to add two numbers together')
+  });
+  it ('Should be able to add two numbers together', function(){
+    console.log('I was able to add three numbers together')
+  });
+  describe('calculate addition with minus numbers', function() {
+    it ('should be able to add -2 and -2 ', function(){
+      console.log('Just added two minus number together')
+    });
+  })
+
+
+});
